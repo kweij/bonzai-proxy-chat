6dc1a27945e9402b803a48c39ccafb62d56d16bd
kweij <klaas.werkt@gmail.com>
2026-02-05 09:38:08 +0100

    Log git hub profile

--- bonzai-proxy-app/src/app/api/auth/[...nextauth]/route.ts
+++ bonzai-proxy-app/src/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,9 @@ import GithubProvider from "next-auth/providers/github"
 const authOptions = {
   callbacks: {
     async signIn({ profile }: { profile: { login: string } }) {
-      return profile.login === "kweij";
+      console.log(profile)
+      // return profile.login === "kweijZzz";
+      return true;
     },
   } as unknown as CallbacksOptions,
   providers: [
