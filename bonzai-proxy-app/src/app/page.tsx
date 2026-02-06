b8e839b6879603c2be791b27a8857c1d45337663
kweij <klaas.werkt@gmail.com>
2026-02-06 13:03:26 +0100

    Typo

--- bonzai-proxy-app/src/app/page.tsx
+++ bonzai-proxy-app/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <div className="inline-flex flex-col items-end">
         <code className="mt-4">process.env.GITHUB_ID = {process.env.GITHUB_ID}</code>
         <code className="mt-4">GITHUB_ID (dev) = <span className="text-green-300">Iv23liubLAT97sui3VVn</span></code>
-        <code className="mt-4">GITHUB_ID (dev) = <span className="text-red-300">Iv23li1DVKYIbTMRDh6j</span></code>
+        <code className="mt-4">GITHUB_ID (prd) = <span className="text-red-300">Iv23li1DVKYIbTMRDh6j</span></code>
       </div>
     </main>
   );
