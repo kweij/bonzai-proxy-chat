6976180e351e24da7d3fa6ac104ca0c6126523b7
kweij <klaas.werkt@gmail.com>
2026-03-31 13:46:21 +0200

    kutvercel

--- bonzai-proxy-app/src/app/page.tsx
+++ bonzai-proxy-app/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <code className="mt-4">process.env.NEXTAUTH_URL = {process.env.NEXTAUTH_URL}</code>
         <code className="mt-4">process.env.GITHUB_ID = {process.env.GITHUB_ID}</code>
         <code className="mt-4">process.env.GITHUB_CLIENT_ID = {process.env.GITHUB_CLIENT_ID}</code>
-        <code className="mt-4">process.env.GITHUB_CLIENT_SECRET(4...) = {process.env.GITHUB_CLIENT_SECRET?.slice(0, 4)...}</code>
+        <code className="mt-4">process.env.GITHUB_CLIENT_SECRET(4...) = {process.env.GITHUB_CLIENT_SECRET?.slice(0, 4)}...</code>
         <code className="mt-4">process.env.VERCEL_FORCE_NO_BUILD_CACHE = {process.env.VERCEL_FORCE_NO_BUILD_CACHE}</code>
       </div>
     </main>
